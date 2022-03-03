d8bbb2532d8883d150967a23afe7003a85e5a549
Antonio Ruiz <antonio.ruiz.rondan@kyndryl.com>
2022-03-03 13:44:02 +0100

    cambios mi bucket9

--- terraform/01-s3.tf
+++ terraform/01-s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "mi_bucket" {
-  bucket = "mi-bucket-prueba-toninoes-v8"
+  bucket = "mi-bucket-prueba-toninoes-v9"
 
   tags = {
     Name        = "Mi bucket bueno de Cadi"
