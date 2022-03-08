9c94700b3683f60253b6f4ed8bbc31f687198d99
Antonio Ruiz Rondán <toninoes@gmail.com>
2022-03-08 19:46:10 +0100

    Delete script.sh

--- terraform/script.sh
+++
@@ -1,7 +0,0 @@
-temp_role=$(aws sts assume-role --role-arn $1 --role-session-name AWS_ROLE_TO_ASSUME)
-export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
-export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
-echo "Current Assume role is:" && aws sts get-caller-identity > /tmp/roles.txt
-terraform init
-terraform apply -auto-approve
