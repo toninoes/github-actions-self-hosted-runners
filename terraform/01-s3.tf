1c0972b9d801815f113ce2376bfde9e7e83ef74c
Antonio Ruiz Rondán <toninoes@gmail.com>
2022-03-09 10:38:26 +0100

    CAMBIO NOMBRE BUCKET

--- terraform/01-s3.tf
+++ terraform/01-s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "mi_bucket" {
-  bucket = "test-runners-toni-alfonso-v1"
+  bucket = "test-runners-toni-alfonso-v2"
 
   tags = {
     Name        = "Mi bucket bueno de Cadi"
