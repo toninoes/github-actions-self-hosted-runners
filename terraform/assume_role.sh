4f30a4f6655cc9b8f3b748124088a85ea21063a9
Antonio Ruiz Rondán <toninoes@gmail.com>
2022-03-09 19:56:58 +0100

    Create assume_role.sh

---
+++ terraform/assume_role.sh
@@ -0,0 +1,5 @@
+temp_role=$(aws sts assume-role --role-arn $$1 --role-session-name aws_tmp_role)
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
+$2
