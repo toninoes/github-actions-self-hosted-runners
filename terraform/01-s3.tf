c81faf11a356728030ccba6bb492bb6cb09680a6
Antonio Ruiz Rondán <toninoes@gmail.com>
2022-03-10 15:59:08 +0100

    Update 01-s3.tf

--- terraform/01-s3.tf
+++ terraform/01-s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "mi_bucket" {
-  bucket = "test-runners-toni-alfonso-v4"
+  bucket = "test-runners-toni-alfonso-v5"
 
   tags = {
     Name        = "Mi bucket bueno de Cadi"
